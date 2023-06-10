d538a74872b3b10e7042b9a2f00ca04427b89414
ekayaint <ekayaint@gmail.com>
2023-06-10 21:50:33 +0300

    First Steps

--- SwiftUI-Animation-Trigger-onDisappear/ContentView.swift
+++ SwiftUI-Animation-Trigger-onDisappear/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showTip = true
+    @State private var showTipButton = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Triggers").font(.largeTitle)
+            Text("On Disappear").font(.title).foregroundColor(.gray)
+            Spacer()
+            
+            if (showTip) {
+                VStack(spacing: 20) {
+                    HStack {
+                        Image(systemName: "info.circle.fill")
+                        Spacer()
+                        Text("Some Info")
+                        Spacer()
+                    }.foregroundColor(.white)
+                    
+                    Spacer()
+                    Button("Ok") {
+                        showTip.toggle()
+                    }
+                }//: VStack
+                .padding(.all,15)
+                .background(Rectangle().fill(Color.green).shadow(radius: 3))
+                .padding(.horizontal,10)
+            }
         }
         .padding()
     }
