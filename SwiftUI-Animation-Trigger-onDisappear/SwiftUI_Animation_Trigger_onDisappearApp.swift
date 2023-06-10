b4fc236b651025721a7801442281a4a21cad8cb9
ekayaint <ekayaint@gmail.com>
2023-06-10 21:34:06 +0300

    Initial Commit

---
+++ SwiftUI-Animation-Trigger-onDisappear/SwiftUI_Animation_Trigger_onDisappearApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_Trigger_onDisappearApp.swift
+//  SwiftUI-Animation-Trigger-onDisappear
+//
+//  Created by ekayaint on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_Trigger_onDisappearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
